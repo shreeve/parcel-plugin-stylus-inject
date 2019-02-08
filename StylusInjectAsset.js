f089eb098cee67826ae52a56e1c945cb07678320
Steve Shreeve <steve.shreeve@gmail.com>
2019-02-08 00:30:56 -0700

    simplify, just modifies code and calls super()

--- StylusInjectAsset.js
+++ StylusInjectAsset.js
@@ -1,30 +1,16 @@
 const StylusAsset = require('parcel-bundler/lib/assets/StylusAsset');
-const localRequire = require('parcel-bundler/src/utils/localRequire');
 const {EOL} = require('os');
 
 class StylusInjectAsset extends StylusAsset {
   async parse(code) {
-    let stylus = await localRequire('stylus', this.name);
     let opts = await this.getConfig(['.stylusrc', '.stylusrc.js'], { packageKey: 'stylus' });
 
     // inject content at beginning of stylus files
     if (opts && opts.inject) {
       code = opts.inject + EOL + code;
     }
-    let style = stylus(code, opts);
-    style.set('filename', this.name);
-    style.set('include css', true);
 
-    // handler for URL function, adjusted to allow pristine references
-    style.define('url', node => {
-      let filename = /^=./.test(node.val) // allow pristine references
-        ? node.val.slice(1)
-        : this.addURLDependency(node.val, node.filename);
-      return new stylus.nodes.Literal(`url(${JSON.stringify(filename)})`);
-    });
-    style.set('Evaluator', await createEvaluator(code, this, style.options));
-
-    return style;
+    return super(code);
   }
 }
 
